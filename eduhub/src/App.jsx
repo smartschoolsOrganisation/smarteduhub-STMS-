e2ff8dd6920efdcb0430e4e0168ee25cd5f3d747
AKOLTINOBRIDGET <2300900107@mubs.ac.ug>
2025-12-15 23:53:31 +0300

    re  installation and environment setup

---
+++ eduhub/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+       <h1 className=" bg-green-500 text-3xl font-bold underline text-red-500">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
