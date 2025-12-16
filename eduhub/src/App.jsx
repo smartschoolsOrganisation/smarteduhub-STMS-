ff5bf9649569e77ab8cd4500085787dd07adf4df
Nambale Kelly <kellyjnambale@gmail.com>
2025-12-16 11:04:17 +0300

    The  light bulb

--- eduhub/src/App.jsx
+++ eduhub/src/App.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 
 function App() {
+  const [isOn, setIsOn] = useState(false);
 
   return (
-    <>
-       <h1 className=" bg-green-500 text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-    </>
-  )
+    <div className="h-screen w-screen bg-white flex flex-col justify-center items-center space-y-10">
+      <div
+        className={
+          isOn
+            ? "h-52 w-52 rounded-full bg-yellow-500 shadow-[0_0_60px_20px_rgba(234,179,8,0.7)]"
+            : "h-52 w-52 rounded-full border"
+        }
+      ></div>
+
+      <button
+        title="Press the button to trigger the bulb"
+        onClick={() => setIsOn(!isOn)}
+        className="border bg-indigo-500 text-white w-52 p-2 rounded-md"
+      >
+        {isOn ? "On" : "Off"}
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
